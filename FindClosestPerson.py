3cf7d02e482d4160929c2480094705f35c8e053d
Gauresh83 <2k23.cscys2311078@gmail.com>
2025-09-04 16:26:50 +0530

    Create  FindClosestPerson.py
    There are three person x  , y  ,  z
    and you have to find which is closest to person z 
    and z is not moving

---
+++ FindClosestPerson.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        d = (x - y) * (x + y - 2 * z)
+        return (d > 0) << 1 | (d < 0)
+      
+
+
+                
+
+   
